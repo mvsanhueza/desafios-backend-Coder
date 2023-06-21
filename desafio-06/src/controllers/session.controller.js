21982d3f28cc23f655a9a2eebef4d9677f09370e
Matias Sanhueza <mvsanhueza@uc.cl>
2023-06-21 15:27:34 -0400

    desafio passport

---
+++ desafio-06/src/controllers/session.controller.js
@@ -0,0 +1,36 @@
+export const testLogin = (req, res, next) =>{
+    const {email, password} = req.body;
+    console.log(email, password);
+    try{
+        if(email === 'adminCoder@coder.com' && password === 'adminCod3r123'){
+            req.session.login = true;
+            res.status(200).json({message: "Usuario logueado"});
+        }
+        else {
+            res.status(401).json({message: "Usuario o contraseña incorrectos"})
+        }
+    }catch (error){
+        res.status(500).json({message: error.message})
+    }
+}
+
+export const destroySession = (req, res, next) =>{
+    if(req.session.login){
+        req.session.destroy(()=>{
+            res.status(200).json({message: "Sesión cerrada"});
+        });
+    }
+}
+
+export const getSession = (req, res, next) =>{
+    if(req.session.login){
+        res.status(200).json({message: "Sesión activa"});
+    }
+    else{
+        res.status(401).json({message: "Sesión no activa"});
+    }
+}
+
+export const signup = (req,res, next) =>{
+    
+}
---
+++ desafio-06/src/routes/session.routes.js
@@ -0,0 +1,71 @@
+import { Router } from "express";
+import userModel from "../models/User.js";
+import { hashData, compareData } from "../utils.js";
+import passport from "passport";
+
+const routerSession = Router();
+
+//login sin passport:
+// routerSession.post('/login', async (req, res) => {
+//     const {email, password} = req.body;
+//     const user = await userModel.findOne({email});
+//     if(!user){
+//         return res.redirect('/api/errorLogin')
+//     }
+
+//     const isPasswordValid = await compareData(password, user.password);
+//     if(!isPasswordValid){
+//         return res.redirect('/api/errorLogin')
+//     }
+
+//     req.session.user = user;
+
+//     res.redirect('/api/products')
+// })
+// login con passport:
+routerSession.post('/login', passport.authenticate('login', { failureRedirect: '/api/errorLogin', successRedirect: '/api/products' }));
+
+routerSession.post('/signup', async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+    if (user) {
+        return res.redirect('/api/errorSignup')
+    }
+    const hashPassword = await hashData(password);
+    const newUser = { ...req.body, password: hashPassword }
+    await userModel.create(newUser);
+    res.redirect('/api')
+})
+
+routerSession.get('/logout', (req, res) => {
+    req.session.destroy(error => {
+        if (error) {
+            console.log(error);
+            res.send(error);
+        }
+        else {
+            res.redirect('/api')
+        }
+    });
+
+})
+
+//github:
+routerSession.get('/githubSignin', passport.authenticate('githubLogin', { scope: ['user:email'] }));
+routerSession.get('/github', passport.authenticate('githubLogin', { failureRedirect: '/api/errorLogin', successRedirect: '/api/products' }));
+
+//facebook:
+routerSession.get('/facebookSignin', passport.authenticate('facebookLogin', { scope: ['email'] }));
+routerSession.get('/facebook', passport.authenticate('facebookLogin', { failureRedirect: '/api/errorLogin', successRedirect: '/api/products' }));
+
+//google:
+routerSession.get('/googleSignin', passport.authenticate('googleLogin', { scope: ['email', 'profile'] }));
+routerSession.get('/google', passport.authenticate('googleLogin', { failureRedirect: '/api/errorLogin', successRedirect: '/api/products' }));
+
+// routerSession.get('/', getSession);
+// routerSession.post('/login', testLogin);
+// routerSession.post('/signup', )
+// routerSession.post('/logout', destroySession);
+
+export default routerSession;
